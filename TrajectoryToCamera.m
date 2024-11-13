077ee9f4ca4c52c43e8718ed11fce4eff1e207bc
JDallin <J.s.dallin56@gmail.com>
2024-11-13 13:45:52 -0700

    John code Update

--- TrajectoryToCamera.m
+++ TrajectoryToCamera.m
@@ -4,26 +4,26 @@ function [position_c] = TrajectoryToCamera(trajectory, cameraobject)
 % Expects aircraft dynamics xyz global frame and return x and y positons in
 % the xz plate of the global frame with +x=>+x and +z=>+y so +y is down
 
-x = cameraobject.x_position;
-y = cameraobject.y_position;
-z = cameraobject.z_position;
-FOV_x = deg2rad(cameraobject.FOV_x);
-FOV_y = deg2rad(cameraobject.FOV_y);
-pix_x = cameraobject.pix_x;
-pix_y = cameraobject.pix_y;
+x = cameraobject.X; % meters
+y = cameraobject.Y;
+z = cameraobject.Z;
+FOV_x = deg2rad(cameraobject.FOV_w); % deg to rad
+FOV_y = deg2rad(cameraobject.FOV_l);
+pix_x = cameraobject.resolution(1); % pixels
+pix_y = cameraobject.resolution(2);
 
 % Performance Optimization
 pix_x_half = pix_x/2;
 pix_y_half = pix_y/2;
 
-phi = deg2rad(cameraobject.phi);
-theta = deg2rad(cameraobject.theta);
-psi = deg2rad(cameraobject.psi);
+phi = deg2rad(cameraobject.attitude(1));
+theta = deg2rad(cameraobject.attitude(2));
+psi = deg2rad(cameraobject.attitude(3));
 
-eul = [psi theta  phi];
-rotmZYX = eul2rotm(eul);
+eul = [phi theta  psi];
+rotmZYX = RotationMatrix321(eul);
 
-trajectory_camera = trajectory - [x y z]; % Ofsets global camer location
+trajectory_camera = trajectory - [x y z]; % Trajectory relative to Camera position
 trajectory_camera = rotmZYX * trajectory_camera'; % Apply Camera Rotation
 trajectory_camera = trajectory_camera';
 [rows, ~] = size(trajectory_camera);
@@ -32,8 +32,8 @@ position_c = zeros(rows,3);
 
 % Uses Johns math to find the x and y camera fame cordinates
 for i = 1:rows
-    t = norm(trajectory_camera(i,1));
-    position_c(i,1) = atan(trajectory_camera(i,2)/t)*pix_x/FOV_x;
+    t = trajectory_camera(i,2);
+    position_c(i,1) = atan(trajectory_camera(i,i)/t)*pix_x/FOV_x;
     position_c(i,2) = atan(trajectory_camera(i,3)/t)*pix_y/FOV_y;
     
     % Sees if the camera could see the pixle
