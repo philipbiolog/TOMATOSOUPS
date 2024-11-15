192875e527bbe6cdf907e87623022e5e653b3113
JDallin <J.s.dallin56@gmail.com>
2024-11-15 02:31:10 -0700

    Full Working
    Everything in this works as intended. I think we can start looking at testing when the lines don't intersect, but right now we are testing when they do because Trajectory... gives us exact and precise values.

--- test3dline.m
+++ test3dline.m
@@ -7,7 +7,7 @@ Pos = [10, 50, 10];
 cameraInfo1.resolution = [3840;2160]; % pixels
 cameraInfo1.FOV_w = 60;
 cameraInfo1.FOV_l = 40; % deg
-cameraInfo1.attitude = [25;0;-50]; % deg
+cameraInfo1.attitude = [15;0;-20]; % deg
 cameraInfo1.X = 10;
 cameraInfo1.Y = 10;
 cameraInfo1.Z = 5; % meters
@@ -16,31 +16,30 @@ cameraInfo1.Z = 5; % meters
 cameraInfo2.resolution = [3840;2160];
 cameraInfo2.FOV_w = 60;
 cameraInfo2.FOV_l = 40;
-cameraInfo2.attitude = [25;0;-130];
+cameraInfo2.attitude = [15;0;20];
 cameraInfo2.X = 60;
-cameraInfo2.Y = 30;
+cameraInfo2.Y = 10;
 cameraInfo2.Z = 5;
 
 Pixel1 = TrajectoryToCamera(Pos, cameraInfo1);
 Pixel2 = TrajectoryToCamera(Pos, cameraInfo2);
 
-[eq1,dBar1] = FirstOrder3Dlines(Pixel1, cameraInfo1);
 
-[eq2,dBar2] = FirstOrder3Dlines(Pixel2, cameraInfo2);
 
-position = SecondOrder3DLines(Pixel1,cameraInfo1,Pixel2,cameraInfo2);
+position = SecondOrder3DLine(Pixel1,cameraInfo1,Pixel2,cameraInfo2);
 
-t = linspace(0,200,2000);
-
-plot3(eq1.X(t),eq1.Y(t),eq1.Z(t))
+t = linspace(0,100,200);
+line1 = Frame2Line(Pixel1, cameraInfo1, t);
+line2 = Frame2Line(Pixel2, cameraInfo2, t);
+plot3(line1(1, :), line1(2, :), line1(3, :), 'r');
 hold on
+plot3(line2(1, :), line2(2, :), line2(3, :), 'b');
 grid on
 plot3(Pos(1), Pos(2), Pos(3), 'ro', 'MarkerSize', 8)
-plot3(eq2.X(t),eq2.Y(t),eq2.Z(t))
 plot3(position(1),position(2),position(3), 'go', 'MarkerFaceColor', 'g', 'MarkerSize', 8)
 xlabel("x axis")
 ylabel("y axis")
 zlabel("z axis")
-% xlim([5 20])
-% ylim([20 60])
-% zlim([5 20])
+xlim([0 80])
+ylim([0 100])
+zlim([0 20])
