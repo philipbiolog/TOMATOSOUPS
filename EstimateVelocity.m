7d5a44b985d22c7cd8a835a1ddb165f67be491cf
Berenger Hickey <behi8856@colorado.edu>
2025-02-05 12:01:31 -0700

    Estimate velocity code

---
+++ EstimateVelocity.m
@@ -0,0 +1,43 @@
+function [raw_data,velocity] = EstimateVelocity(position,time)
+
+raw_vx = diff(position(:,1)) / diff(time);
+raw_vy = diff(position(:,2)) / diff(time);
+raw_vz = diff(position(:,3)) / diff(time);
+
+raw_data = [raw_vx,raw_vy,raw_vz];
+
+px = polyfit(time,raw_data(:,1),5);
+py = polyfit(time,raw_data(:,2),5);
+pz = polyfit(time,raw_data(:,3),5);
+
+vx = polyval(px,time);
+vy = polyval(py,time);
+vz = polyval(pz,time);
+
+velocity = [vx,vy,vz];
+
+figure()
+title('Velocity v Time for All Three Axes')
+subplot(3,1,1)
+plot(time,raw_data(:,1),'ro')
+hold on
+plot(time,vx,'b-')
+xlabel('Time of flight (s)')
+ylabel('X-axis Velocity (m/s)')
+
+subplot(3,1,2)
+plot(time,raw_data(:,2),'ro')
+hold on
+plot(time,vy,'b-')
+xlabel('Time of flight (s)')
+ylabel('Y-axis Velocity (m/s)')
+
+subplot(3,1,2)
+plot(time,raw_data(:,3),'ro')
+hold on
+plot(time,vz,'b-')
+xlabel('Time of flight (s)')
+ylabel('Z-axis Velocity (m/s)')
+
+
+end
---
+++ Final Code/FinalCode.m
@@ -0,0 +1,14 @@
+clear all; clc; close all;
+
+load()
+
+
+
+
+
+
+
+
+
+
+
