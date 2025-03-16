72960e93fbca0b3d4099b2965d383ca8494cb633
Berenger Hickey <behi8856@colorado.edu>
2025-03-16 15:02:21 -0600

    most recent two camera test stuff

--- Two Camera Test/comparison.m
+++ Two Camera Test/comparison.m
@@ -1,11 +1,11 @@
 clear all; clc; close all;
 
 %% VICON data
-VD.test1 = readmatrix("TOMATOSOUPS_Trial.csv");
-VD.test2 = readmatrix("TOMATOSOUPS_Trial 2.csv");
-VD.test3 = readmatrix("TOMATOSOUPS_Trial 3.csv");
-VD.test4 = readmatrix("TOMATOSOUPS_Trial 4.csv");
-VD.test5 = readmatrix("TOMATOSOUPS_Trial 5.csv");
+VD.test1 = readmatrix("TOMATOSOUPS Day2 Trial.csv");
+VD.test2 = readmatrix("TOMATOSOUPS Day2 Trial 1.csv");
+VD.test3 = readmatrix("TOMATOSOUPS Day2 Trial 2.csv");
+VD.test4 = readmatrix("TOMATOSOUPS Day2 Trial 3.csv");
+VD.test5 = readmatrix("TOMATOSOUPS Day2 Trial 4.csv");
 
 names = fieldnames(VD);
 
@@ -20,17 +20,40 @@ end
 
 cam_data = camData();
 
-time = linspace(0,14.75,354)';
+time.test1 = (0:(1/24):(191/24))';
+time.test2 = (0:(1/24):(228/24))';
+time.test3 = (0:(1/24):(358/24))';
+time.test4 = (0:(1/24):(158/24))';
+time.test5 = (0:(1/24):(136/24))';
 
-[position,velocity] = estimation(cam_data.test1,time,2);
 
-plot3(position(:,1),position(:,2),position(:,3))
+
+[positiont1,velocityt1] = estimation(cam_data.test1,time.test1,2);
+[positiont2,velocityt2] = estimation(cam_data.test2,time.test2,2);
+[positiont3,velocityt3] = estimation(cam_data.test3,time.test3,2);
+[positiont4,velocityt4] = estimation(cam_data.test4,time.test4,2);
+[positiont5,velocityt5] = estimation(cam_data.test5,time.test5,2);
+
+
+
+
+
+%%
+figure()
+plot3(positiont1(:,1),positiont1(:,2),positiont1(:,3))
 hold on
 plot3(VICON.test1.pos(:,1),VICON.test1.pos(:,2),VICON.test1.pos(:,3))
 legend('Cam','Vicon')
 xlabel('X-axis')
 ylabel('Y-axis')
 zlabel('Z-axis')
+
+
+figure()
+plot(time.test1,position(:,1))
+
+
+
 
 
 function [position,velocity] = estimation(PixPos,time,n)
@@ -50,11 +73,11 @@ end
 function camInfo = CameraInformation()
 
 % Cam 1
-camInfo.cam1.X = 8.13816;
-camInfo.cam1.Y = 3.9647433024;
-camInfo.cam1.Z = -0.005;
+camInfo.cam1.X = 5.23441573719257125;
+camInfo.cam1.Z = 0.06492045842642645004;
+camInfo.cam1.Y = 2.938185923755515905;
 
-camInfo.cam1.attitude = [-0.11;0;90];
+camInfo.cam1.attitude = [0.5851339;0;89.7897107];
 camInfo.cam1.resolution = [3840;2160];
 
 camInfo.cam1.FOV_w = 107;
@@ -75,47 +98,40 @@ end
 
 function cam = camData()
 
-cam.test1.cam1.x = cell2mat(struct2cell(load('2Cam11.mat','interpX')));
-cam.test1.cam1.z = cell2mat(struct2cell(load('2Cam11.mat','interpY')));
+cam.test1.cam1.x = cell2mat(struct2cell(load('2Cam1Test2Vid1.mat','interpX')));
+cam.test1.cam1.z = cell2mat(struct2cell(load('2Cam1Test2Vid1.mat','interpY')));
 cam.test1.cam1.bool = ones(length(cam.test1.cam1.x),1);
-cam.test1.cam2.x = cell2mat(struct2cell(load('2Cam21.mat','interpX')));
-cam.test1.cam2.z = cell2mat(struct2cell(load('2Cam21.mat','interpY')));
+cam.test1.cam2.x = cell2mat(struct2cell(load('2Cam2Test2Vid1.mat','interpX')));
+cam.test1.cam2.z = cell2mat(struct2cell(load('2Cam2Test2Vid1.mat','interpY')));
 cam.test1.cam2.bool = ones(length(cam.test1.cam2.x),1);
 
-cam.test2.cam1.x = cell2mat(struct2cell(load('2Cam12.mat','interpX')));
-cam.test2.cam1.z = cell2mat(struct2cell(load('2Cam12.mat','interpY')));
+cam.test2.cam1.x = cell2mat(struct2cell(load('2Cam1Test2Vid2.mat','interpX')));
+cam.test2.cam1.z = cell2mat(struct2cell(load('2Cam1Test2Vid2.mat','interpY')));
 cam.test2.cam1.bool = ones(length(cam.test2.cam1.x),1);
-cam.test2.cam2.x = cell2mat(struct2cell(load('2Cam22.mat','interpX')));
-cam.test2.cam2.z = cell2mat(struct2cell(load('2Cam22.mat','interpY')));
+cam.test2.cam2.x = cell2mat(struct2cell(load('2Cam2Test2Vid2.mat','interpX')));
+cam.test2.cam2.z = cell2mat(struct2cell(load('2Cam2Test2Vid2.mat','interpY')));
 cam.test2.cam1.bool = ones(length(cam.test2.cam2.x),1);
 
-cam.test3.cam1.x = cell2mat(struct2cell(load('2Cam13.mat','interpX')));
-cam.test3.cam1.z = cell2mat(struct2cell(load('2Cam13.mat','interpY')));
+cam.test3.cam1.x = cell2mat(struct2cell(load('2Cam1Test2Vid3.mat','interpX')));
+cam.test3.cam1.z = cell2mat(struct2cell(load('2Cam1Test2Vid3.mat','interpY')));
 cam.test3.cam1.bool = ones(length(cam.test3.cam1.x),1);
-cam.test3.cam2.x = cell2mat(struct2cell(load('2Cam23.mat','interpX')));
-cam.test3.cam2.z = cell2mat(struct2cell(load('2Cam23.mat','interpY')));
+cam.test3.cam2.x = cell2mat(struct2cell(load('2Cam2Test2Vid3.mat','interpX')));
+cam.test3.cam2.z = cell2mat(struct2cell(load('2Cam2Test2Vid3.mat','interpY')));
 cam.test3.cam2.bool = ones(length(cam.test3.cam2.x),1);
 
-cam.test4.cam1.x = cell2mat(struct2cell(load('2Cam14.mat','interpX')));
-cam.test4.cam1.z = cell2mat(struct2cell(load('2Cam14.mat','interpY')));
+cam.test4.cam1.x = cell2mat(struct2cell(load('2Cam1Test2Vid4.mat','interpX')));
+cam.test4.cam1.z = cell2mat(struct2cell(load('2Cam1Test2Vid4.mat','interpY')));
 cam.test4.cam1.bool = ones(length(cam.test4.cam1.x),1);
-cam.test4.cam2.x = cell2mat(struct2cell(load('2Cam24.mat','interpX')));
-cam.test4.cam2.z = cell2mat(struct2cell(load('2Cam24.mat','interpY')));
+cam.test4.cam2.x = cell2mat(struct2cell(load('2Cam2Test2Vid4.mat','interpX')));
+cam.test4.cam2.z = cell2mat(struct2cell(load('2Cam2Test2Vid4.mat','interpY')));
 cam.test4.cam2.bool = ones(length(cam.test4.cam2.x),1);
 
-cam.test5.cam1.x = cell2mat(struct2cell(load('2Cam15.mat','interpX')));
-cam.test5.cam1.z = cell2mat(struct2cell(load('2Cam15.mat','interpY')));
+cam.test5.cam1.x = cell2mat(struct2cell(load('2Cam1Test2Vid5.mat','interpX')));
+cam.test5.cam1.z = cell2mat(struct2cell(load('2Cam1Test2Vid5.mat','interpY')));
 cam.test5.cam1.bool = ones(length(cam.test5.cam1.x),1);
-cam.test5.cam2.x = cell2mat(struct2cell(load('2Cam25.mat','interpX')));
-cam.test5.cam2.z = cell2mat(struct2cell(load('2Cam25.mat','interpY')));
+cam.test5.cam2.x = cell2mat(struct2cell(load('2Cam2Test2Vid5.mat','interpX')));
+cam.test5.cam2.z = cell2mat(struct2cell(load('2Cam2Test2Vid5.mat','interpY')));
 cam.test5.cam2.bool = ones(length(cam.test5.cam2.x),1);
-
-cam.test6.cam1.x = cell2mat(struct2cell(load('2Cam16.mat','interpX')));
-cam.test6.cam1.z = cell2mat(struct2cell(load('2Cam16.mat','interpY')));
-cam.test6.cam1.bool = ones(length(cam.test6.cam1.x),1);
-cam.test6.cam2.x = cell2mat(struct2cell(load('2Cam26.mat','interpX')));
-cam.test6.cam2.z = cell2mat(struct2cell(load('2Cam26.mat','interpY')));
-cam.test6.cam2.bool = ones(length(cam.test6.cam2.x),1);
 
 end
 
