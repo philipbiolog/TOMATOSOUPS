616cbccb33b122fcc3948218722a7cb53d888569
Berenger Hickey <behi8856@colorado.edu>
2025-04-14 00:58:10 -0600

    full camera test fr

---
+++ Full System Test/camInfoD1.m
@@ -0,0 +1,92 @@
+function camInfo = camInfoD1()
+
+Rx = RotationMatrix321([90;0;0]);
+
+%% Camera 1 and Camera 8
+% Rotation Matrix 
+R_18 = [0.376080319358385	-0.162522690662849	-0.912222543248632
+-0.599285609886348	0.708200836834624	-0.373240314663203
+0.706696788711003	0.687050179918793	0.168942887092385]; 
+
+% Translation Vector
+t_18 =  [1055.63611107020	-2.17105307418258	959.668468784385]; % Inches
+
+%% Camera 7 and Camera 8
+% Rotation Matrix
+R_78 = [0.999915655411302	0.0106079258734301	-0.00749359540213026
+-0.0106787339356653	0.999898106471831	-0.00947318930338735
+0.00739234096334073	0.00955241240265280	0.999927049695312]; 
+
+% Translation Vector
+t_78 = [-73.7389860631579	7.25821290059708	-19.1142834939380] ; % Inches
+
+%% Camera 1 and Camera 2 
+% Rotation Matrix
+R_12 = [0.471086257738911	-0.0187171968246582	0.881888544155425
+0.0686848083802152	0.997517703145952	-0.0155186664439470
+-0.879408969062359	0.0678829761681997	0.471202468880657];
+
+% Translation Vector
+t_12 = [-1863.74228873381	-119.569422458580	924.048776430168];
+
+%% Cam 1
+camInfo.cam1.X = 0;
+camInfo.cam1.Y = 0;
+camInfo.cam1.Z = 0;
+
+% [0;-5.2324;1.6764]
+
+camInfo.cam1.R = eye(6,6);
+camInfo.cam1.resolution = [3840;2880];
+
+camInfo.cam1.FOV_w = 107.11;
+camInfo.cam1.FOV_l = 74.22; % deg
+
+%% Cam 2
+
+camInfo.cam2.R = Rx*R_12*Rx';
+
+x2 = Rx*t_12;
+
+camInfo.cam2.X = x2(1);
+camInfo.cam2.Y = x2(2);
+camInfo.cam2.Z = x2(3);
+
+camInfo.cam2.resolution = [3840;2880];
+
+camInfo.cam2.FOV_w = 107.11;
+camInfo.cam2.FOV_l = 74.22; % deg
+
+%% Cam 8
+
+x8 = Rx*t_18;
+
+camInfo.cam8.X = x8(1);
+camInfo.cam8.Y = x8(2);
+camInfo.cam8.Z = x8(3);
+
+camInfo.cam8.R = Rx*R_18*Rx';
+
+% camInfo.cam2.attitude = [0;0;92.5];
+camInfo.cam8.resolution = [3840;2880];
+
+camInfo.cam8.FOV_w = 107.11;
+camInfo.cam8.FOV_l = 74.22; % deg
+
+%% Cam 7
+
+x7 = Rx * R_18 * R_78 * t_78 + x8;
+
+camInfo.cam7.X = x7(1);
+camInfo.cam7.Y = x7(2);
+camInfo.cam7.Z = x7(3);
+
+camInfo.cam7.R = Rx*R_18'*R_78*Rx';
+
+% camInfo.cam2.attitude = [0;0;92.5];
+camInfo.cam7.resolution = [3840;2880];
+
+camInfo.cam7.FOV_w = 107.11;
+camInfo.cam7.FOV_l = 74.22; % deg
+
+end
